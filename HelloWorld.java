1a155e4f70047652deda773ecf0fbe80217cc23b
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 20:15:49 -0500

    AND MORE

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 // single line comment
 /* and big comment*/
 /**
@@ -9,9 +10,19 @@ public class HelloWorld {
 	public static void main(String[] args){
 		int myAge= 19;
 		char mySex = 'M';
+		double myHeight = 6.4;
+		String name;
+		Scanner inputDevice = new scanner(system.in);
+		
 		System.out.println("Hello Cis2323!");
 		System.out.println( "I am " + myAge + " years old.");  
-		System.out.println( "My sex is " + mySex);  
+		System.out.print( "I am " + myHeight + " tall.\n");
+		System.out.println( "My sex is " + mySex); 
+		System.out.println("Who might you be, wayward traveler?");
+		name = inputDevice.nextLine();
+		System.out.println("I am " + name + " fair bar keep.");
+		
+		
 	}
 	
 }
