adf4f9907cf068401f57ba0edfbe402cd0a20a7d
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 19:58:33 -0500

    AND MORE

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,8 +7,11 @@
 public class HelloWorld {
 	
 	public static void main(String[] args){
-		
-		System.out.println("Hello Cis2323!");	
+		int myAge= 19;
+		char mySex = 'M';
+		System.out.println("Hello Cis2323!");
+		System.out.println( "I am " + myAge + " years old.");  
+		System.out.println( "My sex is " + mySex);  
 	}
 	
 }
