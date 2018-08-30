bf24864751dbf06e94ebfeade45637ea32fe150a
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 19:37:59 -0500

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+// single line comment
+/* and big comment*/
+/**
+	* Bigger comment
+	*
+*/
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello Cis2323!");	
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# CIS 2323
+
+## WEEK 2 Lab Ex
+
+* Quiz
+* Review
+	* Git
+	* Github
+	* Project layout
+	* make snake with death sides 
+* Primitive var types
+* Input methods
+* OOP
